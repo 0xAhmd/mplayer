c3380cc65b4353d20daae2380a0a3373d793579a
0xAhmd <ahmedhesham200987@gmail.com>
2026-07-14 13:00:03 +0300

    Add duplicate songs to the playlist for testing purposes

--- lib/models/playlist_provider.dart
+++ lib/models/playlist_provider.dart
@@ -9,5 +9,34 @@ class PlaylistProvider extends ChangeNotifier {
       image: "assets/images/playlist1.jpg",
       audioPath: "assets/audio/Data Stream.mp3",
     ),
+    Song(
+      name: "Data Stream",
+      artist: "Doris Roberts",
+      image: "assets/images/playlist1.jpg",
+      audioPath: "assets/audio/Data Stream.mp3",
+    ),
+    Song(
+      name: "Data Stream",
+      artist: "Doris Roberts",
+      image: "assets/images/playlist1.jpg",
+      audioPath: "assets/audio/Data Stream.mp3",
+    ),
+    Song(
+      name: "Data Stream",
+      artist: "Doris Roberts",
+      image: "assets/images/playlist1.jpg",
+      audioPath: "assets/audio/Data Stream.mp3",
+    ),
+    Song(
+      name: "Data Stream",
+      artist: "Doris Roberts",
+      image: "assets/images/playlist1.jpg",
+      audioPath: "assets/audio/Data Stream.mp3",
+    ),
   ];
+
+  int? _currentIndex;
+  // getters
+  List<Song> get playlists => _playlists;
+  int? get currentIndex => _currentIndex;
 }
