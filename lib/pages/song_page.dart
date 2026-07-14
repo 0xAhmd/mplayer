32bf7014883ccc8d8df4f02a280255a7a77f24f0
0xAhmd <ahmedhesham200987@gmail.com>
2026-07-14 15:02:19 +0300

    Enhance SongPage layout with improved UI elements and add playback controls

--- lib/pages/song_page.dart
+++ lib/pages/song_page.dart
@@ -15,38 +15,122 @@ class SongPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(centerTitle: true, title: Text(songName)),
-      body: Column(
-
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(height: 20),
-          Center(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(16),
-              child: Image(
-                fit: BoxFit.cover,
-                image: Image.asset(songCover).image,
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(songName),
+        
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 48),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(24),
+              
+                ),
+                child: Center(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(24),
+                    child: Image.asset(
+                      songCover,
+                      width: 360,
+                      height: 360,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
               ),
-            ),
+              const SizedBox(height: 24),
+              Text(
+                songName,
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 4),
+              Text(
+                songArtist,
+                style:  TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+              ),
+              const SizedBox(height: 32),
+              SliderTheme(
+                data: SliderTheme.of(context).copyWith(
+                  thumbShape: const RoundSliderThumbShape(enabledThumbRadius: 8),
+                  trackShape: const RoundedRectSliderTrackShape(),
+                  overlayShape: const RoundSliderOverlayShape(overlayRadius: 16),
+                ),
+                child: Slider(
+                  activeColor: Theme.of(context).colorScheme.secondary,
+                  value: 0.35,
+                  onChanged: (_) {},
+                  min: 0,
+                  max: 1,
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('1:12'),
+                    Text('3:24'),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    iconSize: 34,
+                    icon: const Icon(Icons.skip_previous_rounded),
+                  ),
+                  const SizedBox(width: 16),
+                  CircleAvatar(
+                    radius: 32,
+                    backgroundColor: Theme.of(context).colorScheme.secondary,
+                    child: IconButton(
+                      onPressed: () {},
+                      iconSize: 32,
+                      icon: const Icon(Icons.play_arrow_rounded, color: Colors.white),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  IconButton(
+                    onPressed: () {},
+                    iconSize: 34,
+                    icon: const Icon(Icons.skip_next_rounded),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 24),
+              Row(
+                children: [
+                  const Icon(Icons.volume_up_rounded),
+                  Slider(
+                    activeColor: Theme.of(context).colorScheme.secondary,
+                    value: 0.7,
+                    onChanged: (_) {},
+                    min: 0,
+                    max: 1,
+                  ),
+                ],
+              ),
+            ],
           ),
-          const SizedBox(height: 24),
-          Padding(
-            padding: const EdgeInsets.only(left: 24,),
-            child: Text(
-              songName,
-              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-          ),
-          const SizedBox(height: 4),
-          Padding(
-            padding: const EdgeInsets.only(left: 24.0,),
-            child: Text(
-              songArtist,
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
--- lib/themes/dark_mode.dart
+++ lib/themes/dark_mode.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 ThemeData darkMode = ThemeData(
   colorScheme:  ColorScheme.dark(
     surface: const Color.fromARGB(255, 27, 27, 27),
-    primary:  Colors.grey.shade600,
+    primary:  const Color.fromARGB(255, 85, 85, 85),
     secondary: Colors.grey.shade800,
     inversePrimary: Colors.grey.shade300,
     
